b0c45333aa33486ea04c4705307214137a08a1c0
Trevor Davis <trevormckay.davis@gmail.com>
2020-05-10 16:18:56 -0500

    removed header, created own header

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
 
   return (
     <div className="App">
+      <Header>Journal</Header>
       <Split>
         <CodeWindow>
           <AceEditor
@@ -40,6 +41,21 @@ function App() {
 }
 
 export default App;
+
+const Header = styled.header`
+  background-color: #191324;
+  color: #75717c;
+  font-size: 0.8rem;
+  height: 23px;
+  text-align: center;
+  position: fixed;
+  box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.2);
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 10;
+  -webkit-app-region: drag;
+`;
 
 const Split = styled.div`
   display: flex;
