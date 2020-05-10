816a19a15d7a473a5c8bcfb9d4a2bf043d25b7fa
Trevor Davis <trevormckay.davis@gmail.com>
2020-05-10 12:49:36 -0500

    displaying md in electron window, removed create-react-app template ui

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import Markdown from "markdown-to-jsx";
 import logo from "./logo.svg";
 import "./App.css";
 
 const { ipcRenderer } = window.require("electron");
 
 function App() {
+  const [loadedFile, setLoadedFile] = useState("");
   useEffect(() => {
     ipcRenderer.on("new-file", (event, fileContent) => {
-      console.log(fileContent);
+      setLoadedFile(fileContent);
     });
   });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Markdown>{loadedFile}</Markdown>
     </div>
   );
 }
