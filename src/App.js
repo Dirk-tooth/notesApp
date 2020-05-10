9424408248ed6fda0e4256ba9ef20b852d2a5a25
Trevor Davis <trevormckay.davis@gmail.com>
2020-05-10 16:10:27 -0500

    added styled components and styled some components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 import Markdown from "markdown-to-jsx";
 import AceEditor from "react-ace";
 import brase from "brace";
@@ -18,18 +19,57 @@ function App() {
 
   return (
     <div className="App">
-      <AceEditor
-        mode="markdown"
-        theme="dracula"
-        onChange={newContent => {
-          setLoadedFile(newContent);
-        }}
-        name="markdown_editor"
-        value={loadedFile}
-      />
-      <Markdown>{loadedFile}</Markdown>
+      <Split>
+        <CodeWindow>
+          <AceEditor
+            mode="markdown"
+            theme="dracula"
+            onChange={newContent => {
+              setLoadedFile(newContent);
+            }}
+            name="markdown_editor"
+            value={loadedFile}
+          />
+        </CodeWindow>
+        <RenderedWindow>
+          <Markdown>{loadedFile}</Markdown>
+        </RenderedWindow>
+      </Split>
     </div>
   );
 }
 
 export default App;
+
+const Split = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+
+const CodeWindow = styled.div`
+  flex: 1;
+  padding-top: 2rem;
+  background-color: #191324;
+`;
+
+const RenderedWindow = styled.div`
+  background-color: #191324;
+  width: 35%;
+  padding: 20px;
+  color: #ffffff;
+  border-left: 1px solid #302b3a;
+  h1,
+  h2,
+  h3,
+  h4,
+  h5. h6 {
+    color: #82d8d8;
+  }
+  h1 {
+    border-bottom: solid 3px #e54b4b;
+    padding-bottom: 10px;
+  }
+  a {
+    color: #e54b4b;
+  }
+`;
