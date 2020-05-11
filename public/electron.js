36b392b02bf62434249d54b8a3707d63f4555c4e
Trevor Davis <trevormckay.davis@gmail.com>
2020-05-11 17:32:30 -0500

    setup build & package, dist -> gitignore, removed localhost dependency

--- public/electron.js
+++ public/electron.js
@@ -2,6 +2,7 @@
 const { app, BrowserWindow, dialog, Menu } = require("electron");
 const path = require("path");
 const fs = require("fs");
+const isDev = require("electron-is-dev");
 
 function createWindow() {
   // Create the browser window.
@@ -16,7 +17,11 @@ function createWindow() {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadURL("http://localhost:3000");
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
